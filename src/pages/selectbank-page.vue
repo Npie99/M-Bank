83244ef4c2f27e90f19799b551d16b78830744ea
Yotsarawoot Chaichana <yots1630@gmail.com>
2023-03-14 00:32:48 +0700

    second commit

---
+++ src/pages/selectbank-page.vue
@@ -0,0 +1,20 @@
+<template>
+    <q-layout view="hHh Lpr fFf">
+      <!-- Be sure to play with the Layout demo on docs -->
+      <div>
+        <q-btn
+          flat
+          class="backPage"
+          icon="bi-arrow-left-circle"
+          to="/home"
+          style="color: #4acb57"
+        />
+        <p class="transferToText">Select Bank</p>
+        <!-- <p class="makeTranText">Make a Transaction</p> -->
+      </div>
+      <!-- <hr class="lineSectionTransfer1" /> -->
+      <!-- <div>
+        <p class="suggestionText">Suggestion</p>
+      </div> -->
+    </q-layout>
+  </template>
